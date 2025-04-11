805c03423fdd58bedb7ae43875d06ee513f32cfb
Anya <81263768+anyabuu@users.noreply.github.com>
2025-04-11 22:36:28 +0300

    refactored, delete function v1

--- src/index.js
+++ src/index.js
@@ -1,117 +1,105 @@
-const form = document.querySelector('form')
-const textarea = document.querySelector('textarea')
-const errorMessage = document.querySelector('#textValidationMessage')
-const cardContainer = document.querySelector('.col-8.offset-2')
+const form = document.querySelector('form');
+const textarea = document.querySelector('textarea');
+const errorMessage = document.querySelector('#textValidationMessage');
+const cardContainer = document.querySelector('.col-8.offset-2');
 
+const route = 'http://localhost:4400';
 
-const route = "http://localhost:4400"
+/**
+ *
+ * @param {string} url
+ * @param {Function} callback
+ * @param {string|undefined} method
+ * @param {any} body
+ */
 
-//method GET
-function getList(){
-  console.log('get list func')
+function sendRequest(url, callback, method = 'GET', body = null) {
+  const request = new XMLHttpRequest();
+  request.open(method, url);
+  request.send(body);
+  request.onload = function () {
+    const response = JSON.parse(request.response);
 
-  cardContainer.innerHTML = ''
+    callback(response);
+  };
+}
 
-  let request = new XMLHttpRequest();
-  request.open('GET', `${route}/api/v1/notes`);
-  request.send()
-  request.onload = function(){
+function validation(textarea, errorMessage) {
+  const minLength = 6;
+  const maxLength = 300;
 
-      JSON.parse(request.response).forEach(function(note){
+  if (textarea.value.length < minLength) {
+    textarea.classList.add('is-invalid');
+    errorMessage.innerText = `Note text must be more than ${minLength} letters`;
 
-      let noteEl = document.createElement('div')
-        noteEl.classList.add('card', 'mb-4')
-        noteEl.dataset.id = note.id
+    return false;
+  } else if (textarea.value.length > maxLength) {
+    errorMessage.innerText = `Note text must be less than ${maxLength} letters`;
 
-      noteEl.innerHTML = `
-          <h5 class="card-header">${note.text}</h5>
+    return false;
+  } else {
+    textarea.classList.remove('is-invalid');
+    return true;
+  }
+}
+
+function deleteItem(item) {
+  const elToDel = document.querySelector(`[data-id="${item.id}"]`);
+  elToDel.remove();
+}
+
+function deleteNote(callback, id) {
+  sendRequest(`${route}/api/v1/notes/${id}`, callback, 'DELETE');
+}
+
+function renderItem(item) {
+  const noteEl = document.createElement('div');
+  noteEl.classList.add('card', 'mb-4');
+  noteEl.dataset.id = item.id;
+
+  noteEl.innerHTML = `
+          <h5 class="card-header">${item.text}</h5>
           <div class="card-body">
             <button class="btn btn-danger">Delete</button>
           </div>
-      `
+      `;
 
-        const deleteButton = noteEl.querySelector('.btn-danger')
-        deleteButton.onclick = function(){
-        console.log('delete')
+  const deleteButton = noteEl.querySelector('.btn-danger');
+  deleteButton.onclick = function () {
+    const { id } = deleteButton.closest('.card').dataset;
 
-          let id = deleteButton.closest('.card').dataset.id
+    deleteNote(deleteItem, id);
+  };
 
-          deleteCard(id)
-        }
-
-
-
-      cardContainer.append(noteEl)
-    })
-
-  }
+  cardContainer.append(noteEl);
 }
 
-getList();
+function renderList(list) {
+  cardContainer.innerHTML = '';
 
-
-
-//method POST
-
-form.onsubmit = function (event){
-  event.preventDefault()
-
-  if(validation(textarea, errorMessage)){
-
-    const formData = new FormData(form)
-    const request = new XMLHttpRequest();
-    request.open("POST", `${route}/api/v1/notes`);
-
-
-    request.send(formData)
-
-    textarea.value = ''
-
-    getList();
-  }
-
+  list.forEach(function (note) {
+    renderItem(note);
+  });
 }
 
-//method DELETE
-
-function deleteCard(id){
-  const request = new XMLHttpRequest();
-  request.open("DELETE", `${route}/api/v1/notes/${id}`);
-  request.send()
-
-  request.onload = function(){
-    getList();
-  }
-
-
+function getNotes(callback) {
+  sendRequest(`${route}/api/v1/notes`, callback);
 }
 
+getNotes(renderList);
 
-
-//validation
-function validation(textarea, errorMessage){
-
-  let minLength = 6;
-  let maxLength = 300;
-
-  if(textarea.value.length < minLength){
-
-    textarea.classList.add('is-invalid')
-    errorMessage.innerText = `Note text must be more than ${minLength} letters`
-
-    return false
-
-  } else if(textarea.value.length > maxLength){
-
-    errorMessage.innerText = `Note text must be less than ${maxLength} letters`
-
-    return false
-
-  } else {
-
-    textarea.classList.remove('is-invalid')
-    return true
-  }
-
+function createNote(callback, body) {
+  sendRequest(`${route}/api/v1/notes`, callback, 'POST', body);
 }
 
+form.onsubmit = function (event) {
+  event.preventDefault();
+
+  if (validation(textarea, errorMessage)) {
+    const formData = new FormData(form);
+
+    createNote(renderItem, formData);
+
+    textarea.value = '';
+  }
+};
