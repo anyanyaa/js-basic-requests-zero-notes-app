95478ebf17a38a089696da93473ac55946b91bc8
Anya <81263768+anyabuu@users.noreply.github.com>
2025-04-23 20:20:54 +0300

    requests promise resolve-reject

--- src/requests.js
+++ src/requests.js
@@ -3,32 +3,58 @@ const route = 'http://localhost:4400';
 /**
  *
  * @param {string} url
- * @param {Function} callback
  * @param {string|undefined} method
  * @param {any} body
  */
 
-export function sendRequest(url, callback, method = 'GET', body = null) {
-  const request = new XMLHttpRequest();
-  request.open(method, url);
-  request.send(body);
-  request.onload = function () {
-    const response = JSON.parse(request.response);
+export function sendRequest(url, method = 'GET', body = null) {
 
-    callback(response);
-  };
+  return new Promise(function (resolve, reject){
+
+    const request = new XMLHttpRequest();
+    request.open(method, url);
+    request.send(body);
+    request.onload = function () {
+
+      const isStatusError = request.status >= 400 && request.status <=500
+
+      if(isStatusError){
+        const error = new Error('server error')
+        error.data = JSON.parse(request.response)
+
+        reject(error)
+        return
+      }
+
+      const response = JSON.parse(request.response);
+
+      resolve(response)
+
+      request.onerror = function (){
+        reject(new Error('network error'))
+      }
+
+    };
+
+  })
 }
 
 export function getNotes(callback) {
-  sendRequest(`${route}/api/v1/notes`, callback);
+  sendRequest(`${route}/api/v1/notes`).then(callback).catch(function (error){
+    console.error(`Impossible to get notes: ${error}`)
+  });
 }
 
 export function createNote(callback, body) {
-  sendRequest(`${route}/api/v1/notes`, callback, 'POST', body);
+  sendRequest(`${route}/api/v1/notes`,'POST', body).then(callback).catch(function(error){
+    console.error(`Impossible to create note: ${error}`)
+  });
 }
 
 export function deleteNote(callback, id) {
-  sendRequest(`${route}/api/v1/notes/${id}`, callback, 'DELETE');
+  sendRequest(`${route}/api/v1/notes/${id}`,'DELETE').then(callback).catch(function(error){
+    console.error(`Impossible to delete note: ${error}`)
+  });
 }
 
 
